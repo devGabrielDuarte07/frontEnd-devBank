63b8c7d5c75e8f19f67d5f627c3f588d433b1bf4
gabrielDuarte <biel111107@gmail.com>
2026-05-13 17:56:55 -0300

    api production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:5059/api'
+    baseURL: 'https://api-devbank.onrender.com'
 })
 
 api.interceptors.request.use(config => {
